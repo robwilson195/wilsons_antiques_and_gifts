0e43bfc64d26dd979ef89169793745ea99cd30c5
Rob Wilson <rob.wilson195@gmail.com>
2018-11-08 16:53:20 +0000

    Set up basic folder directory and db along with sql file to create tables.

---
+++ db/wilsons_antiques_and_gifts.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS gifts;
+DROP TABLE IF EXISTS antiques;
+DROP TABLE IF EXISTS wholesalers;
+DROP TABLE IF EXISTS auctions;
+
+
+CREATE TABLE wholesalers(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255)
+);
+
+CREATE TABLE gifts(
+  id SERIAL8 PRIMARY KEY,
+  name TEXT,
+  description VARCHAR(255),
+  type VARCHAR(255),
+  wholesaler_id INT8 REFERENCES wholesalers(id) ON DELETE CASCADE,
+  purchase_price MONEY,
+  sale_price MONEY,
+  quantity INT4
+);
+
+CREATE TABLE auctions(
+  id SERIAL8 PRIMARY KEY,
+  venue VARCHAR(255),
+  collected_on DATE
+);
+
+CREATE TABLE antiques(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  type VARCHAR(255),
+  auction_id INT8 REFERENCES auctions(id) ON DELETE CASCADE,
+  purchase_price MONEY,
+  sale_price MONEY
+);
