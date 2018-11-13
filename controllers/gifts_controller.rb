f760eae066a01d93336bc088a76b4ce33d59c228
Rob Wilson <rob.wilson195@gmail.com>
2018-11-13 12:53:03 +0000

    Added more routes for gifts.

--- controllers/gifts_controller.rb
+++ controllers/gifts_controller.rb
@@ -7,11 +7,33 @@ require_relative('../models/antique.rb')
 require_relative('../models/auction.rb')
 require_relative('../models/wholesaler.rb')
 require_relative('../models/gift.rb')
+require_relative('../models/type.rb')
 also_reload('../models/*')
 
-# SHOW
+# NEW
+get '/gifts/new' do
+  @wholesalers = Wholesaler.all_by_name
+  @types = Type.all[:gifts]
+  erb (:"gifts/new")
+end
 
+# CREATE
+post '/gifts' do
+  gift = Gift.new(params)
+  gift.save
+  redirect to '/gifts/' + gift.id.to_s
+end
+
+# SHOW
 get '/gifts/:id' do
   @gift = Gift.find(params["id"].to_i)
   erb (:"gifts/show")
+end
+
+# EDIT
+get '/gifts/:id/edit' do
+  @wholesalers = Wholesaler.all_by_name
+  @types = Type.all[:gifts]
+  @gift = Gift.find(params["id"])
+  erb (:"gifts/edit")
 end
